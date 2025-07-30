64040c9a760c93f73537460570a8b4796e9a893d
J241918 <J241918@tafe.wa.edu.au>
2025-07-30 22:35:10 +0800

    New code js

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,14 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
+let lastNumberElement = numbers.pop();
 
 // Use the unshift() method to add the last element to the beginning of the array
+numbers.unshift(lastNumberElement);
 
 // Use the slice() method to exclude the first 3 elements and return a new array
+let newNumbersArrary = numbers.slice(3);
 
 // Set the value of the output field inner html to the new array
+let outputField = document.getElementById('OutputField');
+outputField.innerHTML =newNumbersArrary;
